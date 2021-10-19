3ad71cc6362248350b48ee72b7295d5836c87d84
Denice Omene <dmo223@lehigh.edu>
2021-10-19 04:12:56 -0400

    amplify with vue files

---
+++ amplifyauth/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div v-if="authState !== 'signedin'">You are signed out.</div>
+    <amplify-authenticator>
+      <div v-if="authState === 'signedin' && user">
+        <div>Hello, {{user.username}}</div>
+      </div>
+      <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
+  </div>
+</template>
+
+<script>
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
+
+export default {
+  name: 'AuthApp',
+  data() {
+    return {
+      user: undefined,
+      authState: undefined,
+      unsubscribeAuth: undefined
+    }
+  },
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    })
+  },
+  
+  beforeDestroy() {
+    this.unsubscribeAuth();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
